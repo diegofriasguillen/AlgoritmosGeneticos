3c1dbfa7639c4f43f88f79ac22ae754473ea351c
Diego <diego.frias@artek.edu.mx>
2024-05-02 12:49:05 -0600

    MAP

---
+++ Assets/Scripts/Animals.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animals
+{
+    private Genes _genes;
+}
+
+public class Genes
+{
+    private float _height;
+    private float _straight;
+}
+
